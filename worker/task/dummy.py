f238000b800a30a7724d02c68c5ffd714ab5a91f
ChoneungSon <60080290+ChoneungSon@users.noreply.github.com>
2024-04-11 14:39:08 +0900

    asyncio run

--- worker/task/dummy.py
+++ worker/task/dummy.py
@@ -1,22 +1,27 @@
 import os
-from time import sleep
 from datetime import datetime
 from worker.worker import app
 from worker.middleware import OnFailureTask
+
+import asyncio
 
 
 @app.task(base=OnFailureTask, priority=0)
 def dummy_task(t: int):
     folder = "celery"
     os.makedirs(folder, exist_ok=True)
-    sleep(t)
+    asyncio.run(asyncio.sleep(t))
     now = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
     with open(f"{folder}/{now}.txt", "w") as f:
         f.write("Hello, world!")
 
 
 # authoretry_for=(ValueError,)
-@app.task(base=OnFailureTask, priority=5, retry_kwargs={"max_retries": 5})
-def dummy_fail(t: int):
-    sleep(t)
-    raise ValueError("This task always fails.")
+# rate_limit='100/m'
+@app.task(bind=True, base=OnFailureTask, priority=5, max_retries=5)
+def dummy_fail(self, t: int):
+    asyncio.run(asyncio.sleep(t))
+    try:
+        raise ValueError("This task always fails.")
+    except Exception as e:
+        raise self.retry(exc=e, countdown=10, max_retries=5)
